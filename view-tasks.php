a700957709a55371a7fb3e17f13e6d9207d9b584
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-11-06 14:40:30 -0600

    Create view-tasks.php

---
+++ view-tasks.php
@@ -0,0 +1,15 @@
+<h1>Tasks</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <th>ID</th>
+      <th>Title</th>
+      <th>Status/th>
+    </thead>
+    <tbody>
+<?php
+while ($tasks
+?>
+    </tbody>
+  </table>
+</div>
