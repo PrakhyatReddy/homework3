2b12e897de70b0ce986371ce4a0177dc6f0044af
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-11-06 15:13:58 -0600

    Update model-comments.php

--- model-comments.php
+++ model-comments.php
@@ -2,7 +2,9 @@
 function selectComments() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT comment_id, task_id, comment_txt FROM `Comments`");
+        $stmt = $conn->prepare("SELECT comment_id, title, comment_txt
+FROM Tasks
+JOIN Comments ON Tasks.task_id = Comments.task_id;");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
