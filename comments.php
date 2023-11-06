4adee1af21509ac8d3714f09288ded8607532371
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-11-06 15:06:12 -0600

    Update comments.php

--- comments.php
+++ comments.php
@@ -1,1 +1,10 @@
-
+<?php
+require_once("util-db.php");
+require_once("model-comments.php");
+  
+$pageTitle ="Comments";
+include "view-header.php";
+$comments = selectComments(); 
+include "view-comments.php";
+include "view-footer.php";
+?>
