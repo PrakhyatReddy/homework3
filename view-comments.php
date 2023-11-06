17824dfc4d27f9ec79d9782c9f99c2e9568b31af
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-11-06 15:13:37 -0600

    Create view-comments.php

---
+++ view-comments.php
@@ -0,0 +1,25 @@
+<h1>Comments</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Task ID</th>
+        <th>Commment</th> 
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($comment = $comments->fetch_assoc()){
+  ?>
+  <tr>
+    <td><?php echo $comment['comment_id']; ?></td>
+    <td><?php echo $comment['title']; ?></td>
+    <td><?php echo $comment['comment_txt']; ?></td>
+  </tr>
+<?php
+}
+?>
+    </tbody>
+  </table>
+</div>
