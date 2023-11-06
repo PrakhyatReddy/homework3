063c8c15f1b05028c1e4315f7e47532df97118b7
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-11-06 15:24:45 -0600

    Update tasks-with-comments.php

--- tasks-with-comments.php
+++ tasks-with-comments.php
@@ -1,1 +1,10 @@
-
+<?php
+require_once("util-db.php");
+require_once("model-tasks-with-comments.php");
+  
+$pageTitle ="Tasks with Comments";
+include "view-header.php";
+$taskswithcomments = selectTasksWithComments($_GET['id']); 
+include "view-tasks-with-comments.php";
+include "view-footer.php";
+?>
