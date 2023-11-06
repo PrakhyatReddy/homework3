5894e04e9a9a5ff0abcb3f1ac13ee3b36d964bd4
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-11-06 16:22:03 -0600

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,8 @@ include "view-header.php";
 <div class="container text-center my-5">
     <h1>Welcome to Homework 3</h1>
     <p>Your one-stop solution for managing tasks, comments, and assignments for Homework 3.</p>
-    <a href="tasks-with-comments.php" class="btn btn-primary mt-3">Get Started</a>
+    <a href="tasks.php" class="btn btn-primary mt-3">Get Started</a>
+    <a href="comments.php" class="btn btn-primary mt-3">Get Started</a>
 </div>
 
 <?php
