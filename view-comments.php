c5975edb8eba8bde3d81db4ec177f46f396de5e8
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-11-06 16:56:10 -0600

    Update view-comments.php

--- view-comments.php
+++ view-comments.php
@@ -6,6 +6,7 @@
         <th>ID</th>
         <th>Task</th>
         <th>Commment</th> 
+        <th></th>
       </tr>
     </thead>
     <tbody>
@@ -16,6 +17,12 @@ while ($comment = $comments->fetch_assoc()){
     <td><?php echo $comment['comment_id']; ?></td>
     <td><?php echo $comment['title']; ?></td>
     <td><?php echo $comment['comment_txt']; ?></td>
+    <td>
+      <form method ="post" action="comments-by-user.php">
+        <input type="hidden" name="cid" vaLue="<?php echo $comment['comment_id']; ?>">
+          <button type="submit" class="btn btn-primary">Users</button>
+      </form>
+    </td>
   </tr>
 <?php
 }
