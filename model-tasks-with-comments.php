e7f1871d5e4d3e8c45fd9096a3f5d7f81dbf7d41
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-11-06 15:50:05 -0600

    Update model-tasks-with-comments.php

--- model-tasks-with-comments.php
+++ model-tasks-with-comments.php
@@ -1,5 +1,5 @@
 <?php
-function selectTasksWithComments() {
+function selectTasksWithComments($taskid) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT
@@ -14,6 +14,7 @@ JOIN Comments c ON t.task_id = c.task_id
 JOIN assignment a ON t.task_id = a.task_id
 JOIN user u ON a.user_id = u.user_id
 WHERE t.task_id=?");
+        $stmt->bind_param("i", $taskid);
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
