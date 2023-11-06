74c0ecbbf1533bfd94b2225d51211564ce14d67e
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-11-06 14:44:39 -0600

    Update view-tasks.php

--- view-tasks.php
+++ view-tasks.php
@@ -2,13 +2,23 @@
 <div class="table-responsive">
   <table class="table">
     <thead>
-      <th>ID</th>
-      <th>Title</th>
-      <th>Status/th>
+      <tr>
+        <th>ID</th>
+        <th>Title</th>
+        <th>Status/th> 
+      </tr>
     </thead>
     <tbody>
 <?php
-while ($tasks
+while ($task = $tasks->fetch_assoc()){
+  ?>
+  <tr>
+    <td><?php echo $task['task_id']; ?></td>
+    <td><?php echo $task['title']; ?></td>
+    <td><?php echo $task['status']; ?></td>
+  </tr>
+<?php
+}
 ?>
     </tbody>
   </table>
