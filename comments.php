c5bdbc0987ca31df3fd32d51238ed8915879ae60
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-11-07 16:04:01 -0600

    Update comments.php

--- comments.php
+++ comments.php
@@ -14,6 +14,14 @@ if (isset($_POST['actionType'])){
         echo '<div class="alert alert-danger" role="alert" Error </div>';
       }
       break;
+
+        case "Delete":
+      if(deleteComments($_POST['cid'])){
+        echo '<div class="alert alert-success" role="alert" Comment Deleted </div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert" Error </div>';
+      }
+      break;
   }
 }
 
