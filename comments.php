6336d390f2abcc6a547c9af2434395ca131b9610
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-11-07 16:06:04 -0600

    Update comments.php

--- comments.php
+++ comments.php
@@ -9,17 +9,17 @@ if (isset($_POST['actionType'])){
   switch($_POST['actionType']){
     case "Add":
       if(insertComments($_POST['cNumber'], $_POST['cDesc'])){
-        echo '<div class="alert alert-success" role="alert" Comment Added </div>';
+        echo '<div class="alert alert-success" role="alert"> Comment Added </div>';
       } else {
-        echo '<div class="alert alert-danger" role="alert" Error </div>';
+        echo '<div class="alert alert-danger" role="alert"> Error </div>';
       }
       break;
 
         case "Delete":
       if(deleteComments($_POST['cid'])){
-        echo '<div class="alert alert-success" role="alert" Comment Deleted </div>';
+        echo '<div class="alert alert-success" role="alert"> Comment Deleted </div>';
       } else {
-        echo '<div class="alert alert-danger" role="alert" Error </div>';
+        echo '<div class="alert alert-danger" role="alert"> Error </div>';
       }
       break;
   }
