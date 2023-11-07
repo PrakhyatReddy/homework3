1884eb36030c9d98270b6a684cd88e49619a45f3
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-11-07 14:54:18 -0600

    Update model-comments.php

--- model-comments.php
+++ model-comments.php
@@ -14,4 +14,46 @@ JOIN Comments ON Tasks.task_id = Comments.task_id;");
         throw $e;
     }
 }
+
+function insertComments($cTask, $cDesc) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `Comments` (`task_id`, `comment_txt`) VALUES (?, ?)");
+        $stmt->bind_param("ss", $cTask, $cDesc);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateComments($cid, $cTask, $cDesc) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `Comments` set `task_id` =?, `comment_txt` =? where comment_id =?");
+        $stmt->bind_param("iss", $cid, $cTask, $cDesc);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteComments($cid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from Comments where comment_id=?");
+        $stmt->bind_param("i", $cid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
