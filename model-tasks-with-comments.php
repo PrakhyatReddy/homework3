a92bf90c8ec4ac336e53991ed29d14d9a006bd9a
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-11-07 12:33:57 -0600

    Update model-tasks-with-comments.php

--- model-tasks-with-comments.php
+++ model-tasks-with-comments.php
@@ -1,27 +1,33 @@
 <?php
-function selectTasksWithComments($task_id) {
-    try {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT
-    t.task_id,
-    t.title,
-    c.comment_txt,
-    u.user_name,
-    u.user_email,
-    a.user_id
-FROM Tasks t
-JOIN Comments c ON t.task_id = c.task_id
-JOIN assignment a ON t.task_id = a.task_id
-JOIN user u ON a.user_id = u.user_id
-WHERE t.task_id=?");
-        $stmt->bind_param("i", $task_id);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $conn->close();
-        return $result;
-    } catch (Exception $e) {
-        $conn->close();
-        throw $e;
+function selectTasksWithComments($id) {
+    require("util-db.php");
+    
+    // Define the base SQL query
+    $sql = "SELECT t.task_id, t.title, c.comment_txt, u.user_name, u.user_email, t.status FROM Tasks t
+            JOIN Comments c ON t.task_id = c.task_id
+            JOIN assignment a ON t.task_id = a.task_id
+            JOIN user u ON a.user_id = u.user_id";
+    
+    // Check if 'id' is set and modify the query accordingly
+    if ($id > 0) {
+        $sql .= " WHERE t.task_id = $id";
     }
+    
+    $result = $conn->query($sql);
+    
+    if (!$result) {
+        die("Error executing the query: " . $conn->error);
+    }
+    
+    $data = array();
+    
+    while ($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+    
+    $conn->close();
+    
+    return $data;
 }
+
 ?>
