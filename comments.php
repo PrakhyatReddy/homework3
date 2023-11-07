1d85a3965afcdfa198aef0ab02c2810f2b48f027
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-11-07 15:33:57 -0600

    Update comments.php

--- comments.php
+++ comments.php
@@ -4,6 +4,15 @@ require_once("model-comments.php");
   
 $pageTitle ="Comments";
 include "view-header.php";
+
+if (isset($_POST['actionType'])){
+  switch($_POST['actionType']){
+    case "Add":
+      insertComment($_Post['cTask'], $_Post['cDesc']);
+      break;
+  }
+}
+
 $comments = selectComments(); 
 include "view-comments.php";
 include "view-footer.php";
