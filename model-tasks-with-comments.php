4394fcd0d080f3869f1409b698a58a0928f0416b
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-11-07 12:36:52 -0600

    Update model-tasks-with-comments.php

--- model-tasks-with-comments.php
+++ model-tasks-with-comments.php
@@ -2,27 +2,25 @@
 function selectTasksWithComments($task_id) {
     try {
         $conn = get_db_connection();
-        $sql = "SELECT
-            t.task_id,
-            t.title,
-            c.comment_txt,
-            u.user_name,
-            u.user_email,
-            a.user_id
-        FROM Tasks t
-        JOIN Comments c ON t.task_id = c.task_id
-        JOIN assignment a ON t.task_id = a.task_id
-        JOIN user u ON a.user_id = u.user_id
-        WHERE t.task_id = :task_id";
-        
-        $stmt = $conn->prepare($sql);
-        $stmt->bindParam(':task_id', $task_id, PDO::PARAM_INT);
+        $stmt = $conn->prepare("SELECT
+    t.task_id,
+    t.title,
+    c.comment_txt,
+    u.user_name,
+    u.user_email,
+    a.user_id
+FROM Tasks t
+JOIN Comments c ON t.task_id = c.task_id
+JOIN assignment a ON t.task_id = a.task_id
+JOIN user u ON a.user_id = u.user_id
+WHERE t.task_id=?");
+        $stmt->bind_param("i", $task_id);
         $stmt->execute();
-        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        $conn = null;  // Close the database connection
-
+        $result = $stmt->get_result();
+        $conn->close();
         return $result;
-    } catch (PDOException $e) {
+    } catch (Exception $e) {
+        $conn->close();
         throw $e;
     }
 }
