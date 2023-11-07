4320d73c29899dba8f999305f70ce7325cfb11d0
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-11-07 12:29:22 -0600

    Update tasks-with-comments.php

--- tasks-with-comments.php
+++ tasks-with-comments.php
@@ -3,8 +3,13 @@ require_once("util-db.php");
 require_once("model-tasks-with-comments.php");
   
 $pageTitle ="Tasks with Comments";
-$taskswithcomments = selectTasksWithComments($_GET['id']); 
 include "view-header.php";
+
+// $taskswithcomments = selectTasksWithComments($_GET['id']); 
+
+$id = isset($_GET['id']) ? $_GET['id'] : 0; // Check if 'id' is set, provide a default value if not
+$taskswithcomments = selectTasksWithComments($id); 
+
 include "view-tasks-with-comments.php";
 include "view-footer.php";
 ?>
