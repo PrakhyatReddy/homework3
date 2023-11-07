1880ef7fb04354786f4e8e508a720600ab382842
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-11-07 12:46:12 -0600

    Update tasks-with-comments.php

--- tasks-with-comments.php
+++ tasks-with-comments.php
@@ -7,8 +7,11 @@ include "view-header.php";
 
 // $taskswithcomments = selectTasksWithComments($_GET['id']); 
 
-$id = isset($_GET['id']) ? $_GET['id'] : 0; // Check if 'id' is set, provide a default value if not
-$taskswithcomments = selectTasksWithComments($id); 
+// $id = isset($_GET['id']) ? $_GET['id'] : [1:6]; // Check if 'id' is set, provide a default value if not
+// $taskswithcomments = selectTasksWithComments($id); 
+
+$id = isset($_GET['id']) && in_array($_GET['id'], range(1, 6)) ? $_GET['id'] : range(1, 6);
+$taskswithcomments = selectTasksWithComments($id);
 
 include "view-tasks-with-comments.php";
 include "view-footer.php";
