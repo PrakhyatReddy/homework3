614fb33e977e60ab46fd41de35e2f09410a3533f
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-11-07 12:22:06 -0600

    Update comments-by-user.php

--- comments-by-user.php
+++ comments-by-user.php
@@ -2,7 +2,7 @@
 require_once("util-db.php");
 require_once("model-comments-by-user.php");
   
-$pageTitle ="Comments by User";
+$pageTitle ="Status by Comment";
 include "view-header.php";
 $commentbyuser = selectCommentsByUser($_POST['cid']); 
 include "view-comments-by-user.php";
